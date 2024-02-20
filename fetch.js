f8088141b0627e16f592f21e9a71b10f88a0efc9
Umar Saleem <35795385+umersaleem78@users.noreply.github.com>
2024-02-20 19:54:26 +0500

    updated

--- fetch.js
+++ fetch.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
-const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
+const MEDIUM_USERNAME = "";
 
 const ERR = {
   noUserName:
